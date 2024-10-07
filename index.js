0faaebbb78209a2bdf64cc7244bab1ea8c5fdb08
Gulzarokz <gokz22301@gamil.com>
2024-10-07 10:13:55 +0500

    done javascritp 3 exercise

---
+++ index.js
@@ -0,0 +1,72 @@
+// chpater No 1 alert
+// 
+// what is an alert
+// it is key words
+// can be used single or double quotes
+// alert('Hello world');
+// alert("Thanks for your input!");
+// window.alert("Thanks for your input!"); window is global objects
+// alert("your Registration has been completed");
+
+
+// chpater No 2 Variable for string
+
+// var gradeFiveBook = "MY Five grade book";
+// console.log(gradeFiveBook);
+
+
+//  var vs let
+// function hello(){
+//     {
+//         // var secret = "it is my secret";
+//         let secret = "it is my secret";
+//         console.log("ya to access hogya: " +secret);
+        
+//     }
+//     {
+//         // console.log("ya to access hogya: " +secret);
+//     }
+// }
+// hello();
+
+
+// const vs let
+
+// const PI = 3.14
+// PI = 4.6;
+// alert(PI);
+
+// difference b/w = , ==, ===;
+
+// if(1 === "b"){
+// console.log("barabar");
+
+// }else{
+//     console.log("nahi g ");
+    
+// }
+
+// chapter No 3 variable for Number
+
+// var mySalary = 300;
+// console.log(mySalary);
+
+// const myHeight = 9.4;
+// console.log(myHeight);
+
+// let marks = 90;
+// tot = marks + 10;
+// console.log(tot);
+
+// let chocalate = 50;
+// let newBox = chocalate + 20;
+// console.log(newBox);
+
+
+// const Number1 = 10;
+// const Number2 =
+//  90;
+// console.log(Number1 + Number2);
+
+
+
